762ab987042ccebc99a4983ef74bffb9f36dd6da
danil <natalya.haritonov.72@gmail.com>
2023-10-25 14:04:04 +0300

    Initial commit

---
+++ app/src/main/java/com/danilkharytonov/composecontacts/data/model/User.kt
@@ -0,0 +1,10 @@
+package com.danilkharytonov.composecontacts.data.model
+
+data class User(
+    var name: String,
+    var surname: String,
+    var phoneNumber: String,
+    var email: String,
+    var dateOfBirth: String,
+    var imageIcon: String
+)
